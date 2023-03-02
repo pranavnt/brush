9b04851b3b408232e5658004795a9fdefe45f68d
Steven He <s-sthe@lwsd.org>
2023-03-02 13:50:29 -0800

    Tokenize some basic symbols

--- src/tokens.rs
+++ src/tokens.rs
@@ -12,12 +12,18 @@
     type: "NUMBER",
     value: "2"
 }*/
-
+#[derive(Debug)]
 pub enum TokenType {
-    // go through the language and find all the tokens
+    L_CURLY,
+    R_CURLY,
+    L_PAREN,
+    R_PAREN,
+    IDENTIFIER,
     NUMBER,
     OPERATOR,
     SHIFT_KEYWORD,
+    STRETCH_KEYWORD,
+    ROTATE_KEYWORDS
 }
 
 pub struct Token {
@@ -36,5 +42,27 @@ impl Token {
 
 pub fn code_to_token(input: String) {
     let mut lines = input.split("\n");
-    println!("{:#?}", lines.next());
+    let mut all_tokens = Vec::<Token>::new();
+
+    for line in lines {
+        let mut chars = line.chars().peekable();
+
+        while let Some(cc) = chars.next() {
+            match cc {
+                // Match current character to left and right parens and curly braces
+                '{' => all_tokens.push(Token::new(TokenType::L_CURLY, cc.to_string())),
+                '}' => all_tokens.push(Token::new(TokenType::R_CURLY, cc.to_string())),
+                '(' => all_tokens.push(Token::new(TokenType::L_PAREN, cc.to_string())),
+                ')' => all_tokens.push(Token::new(TokenType::R_PAREN, cc.to_string())),
+
+                '+' | '-' | '*' | '/' => all_tokens.push(Token::new(TokenType::OPERATOR,cc.to_string())),
+
+                _ => ()
+            }
+        }
+    }
+
+    for token in all_tokens {
+        println!("{:?} : {}", token.token_type, token.value);
+    }
 }
