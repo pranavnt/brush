9825374bbb31924377d127eea58fde727b03321b
Steven He <s-sthe@lwsd.org>
2023-03-09 12:54:42 -0800

    Printing AST to console

--- src/ast.rs
+++ src/ast.rs
@@ -63,48 +63,4 @@ impl Node {
             children: vec![],
         }
     }
-
-    pub fn to_string(&self) -> String {
-        let mut string = String::new();
-
-        match self.node_type {
-            NodeType::Program => {
-                for child in &self.children {
-                    string.push_str(&child.to_string());
-                }
-            }
-            NodeType::ShapeDeclaration => {
-                string.push_str("ShapeDeclaration \n");
-            }
-            NodeType::Shape(ref shape_type) => {
-                string.push_str("Shape \n");
-            }
-            NodeType::Property(ref identifier) => {
-                string.push_str("Property \n");
-            }
-            NodeType::DrawShape => {
-                string.push_str("DrawShape \n ");
-            }
-            NodeType::MathExpression => {
-                string.push_str(&format!("MathExpression({}) \n", self.value.as_ref().unwrap().value));
-            }
-            NodeType::NumberLiteral => {
-                string.push_str(&format!("NumberLiteral({}) \n", self.value.as_ref().unwrap().value));
-            }
-            NodeType::Identifier => {
-                string.push_str(&format!("Identifier({}) \n", self.value.as_ref().unwrap().value));
-            }
-            NodeType::Operator => {
-                string.push_str(&format!("Operator({}) \n", self.value.as_ref().unwrap().value));
-            }
-            NodeType::ShapeProperty => {
-                string.push_str(&format!("ShapeProperty() \n"));
-            }
-            NodeType::ShapeIdentifier => {
-                string.push_str(&format!("ShapeIdentifier({}) \n", self.value.as_ref().unwrap().value));
-            }
-        }
-
-        string
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn main() {
 
                 let ast = parser.parse();
 
-                println!("{:#?}", ast.to_string());
+                println!("{:#?}", ast);
 
                 let interpreter = Interpreter::new(ast);
 
