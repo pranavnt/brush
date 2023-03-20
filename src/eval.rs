31790d6bd496ff81398d089c5c827631fb365249
Pranav Teegavarapu <pranavnt@outlook.com>
2023-03-19 17:43:33 -0700

    call stack

--- src/eval.rs
+++ src/eval.rs
@@ -1,22 +1,33 @@
+use std::collections::*;
 use crate::ast::{Node};
 use crate::tokens::{Token, TokenType};
 
 pub struct Interpreter {
     pub ast: Node,
+    pub symbol_table: HashMap<String, Node>,
+    pub call_stack: Vec<Node>,
 }
 
 impl Interpreter {
     pub fn new(ast: Node) -> Interpreter {
         Interpreter {
             ast: ast,
+            symbol_table: HashMap::new(),
+            call_stack: Vec::new(),
         }
     }
 
     pub fn run(&self) {
-        println!("Interpreting AST");
+        // iterate through statements
+
+        // shape declaration statements
+
+        // shape draw statements
     }
 
-    pub fn eval_node(&mut self) {
-        unimplemented!();
+    pub fn eval(&mut self, node: Node) {
+        match node {
+
+        }
     }
 }
