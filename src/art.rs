1ca4e735fccfdd1d8fafc277e909e0ca26dbd478
Steven He <s-sthe@lwsd.org>
2023-03-21 12:50:38 -0700

    Skeleton for capturing path transformations

--- src/art.rs
+++ src/art.rs
@@ -1,27 +1,30 @@
+use std::ops::DerefMut;
+use std::ptr::addr_of;
+
 use svg::Document;
 use svg::parser::Event;
-use svg::node::element::Path;
+use svg::node::element::{Path, Line};
 use svg::node::element::tag::Path;
-use svg::node::element::path::{Command, Data};
+use svg::node::element::path::{Command, Data, Parameters};
 use crate::error::Error;
 
 pub struct Shape {
     pub svg: Path,
     path: Data,
-    center: (f64, f64),
-    dimensions: (f64, f64),
+    center: (f32, f32),
+    dimensions: (f32, f32),
     fill: (u8, u8, u8),
     outline_color: (u8, u8, u8),
-    outline_width: f64,
-    rotation: f64,
-    stretch: (f64, f64),
-    warp: f64,
+    outline_width: f32,
+    rotation: f32,
+    stretch: (f32, f32),
+    warp: f32,
 }
 
 impl Shape {
     pub fn new_circle(
-        center: (f64, f64),
-        radius: f64,
+        center: (f32, f32),
+        radius: f32,
     ) -> Shape {  
         let mut path = Data::new();
         let mut x = center.0 + radius;
@@ -31,7 +34,7 @@ impl Shape {
         while angle < 360.0 {
             // path.move_to defines the starting point of the path
             // path.line_to defines 
-            // path.move_to((x, y));
+            // path = path.move_to((x, y));
             // let delta_x = radius * angle.cos();
             // let delta_y = radius * angle.sin();
             // path.line_to((center.0 + delta_x, center.1 + delta_y));
@@ -67,14 +70,70 @@ impl Shape {
         unimplemented!();
     }
 
-    pub fn shift(&mut self, x: f64, y: f64) {
+    pub fn shift(&mut self, x: f32, y: f32) {
         self.center.0 += x;
         self.center.1 += y;
 
-        // iterate through the path and shift each point   
+        // iterate through the path and shift each point
+        let mut cdata = self.path.clone();
+        let mut newData = Data::new();
+        
+        // bruh we have to handle each type of command
+        for cmd in cdata.iter() { // derefererence error here
+            match cmd {
+                Command::Move(pos, para) => {
+                    newData = newData.move_to((para.get(0).unwrap() + x, para.get(1).unwrap() + y));
+                }
+
+                Command::Line(pos, para) => {
+                    newData = newData.move_to((para.get(0).unwrap() + x, para.get(1).unwrap() + y));
+                }
+
+                Command::HorizontalLine(pos, para) => {
+                    unimplemented!();
+                }
+
+                Command::VerticalLine(pos, para) => {
+                    unimplemented!();
+                }
+
+                Command::QuadraticCurve(pos, para) => {
+                    unimplemented!();
+                }
+
+                Command::SmoothQuadraticCurve(pos, para) => {
+                    unimplemented!();
+                }
+
+                Command::SmoothCubicCurve(pos, para) => {
+                    unimplemented!();
+                }
+
+                Command::EllipticalArc(pos, para) => {
+                    unimplemented!();
+                }
+
+                Command::CubicCurve(pos, para) => {
+                    unimplemented!();
+                }
+
+                Command::Close => {}
+            }
+        }
+
+        self.path = newData.close();
+
+        // need to recompute svg Path based on transformed Data
+
+        // self.svg = Path::new()
+        // .set("fill", "none")
+        // .set("stroke", "black")
+        // .set("stroke-width", 1)
+        // .set("d", self.path);
+
     }
 
-    pub fn rotate(&mut self, angle: f64) {
+    pub fn rotate(&mut self, angle: f32) {
         self.rotation += angle;
 
         // iterate through the path and rotate each point around the center
