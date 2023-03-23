62490307e12e2665464aa0769aadfbac93222ce8
Suyash Mothukuri <s-smothukuri@lwsd.org>
2023-03-23 12:59:00 -0700

    Update eval.rs

--- src/eval.rs
+++ src/eval.rs
@@ -7,7 +7,6 @@ pub struct Interpreter {
     pub ast: ProgramNode,
     pub symbol_table: HashMap<String, Value>,
     pub call_stack: Vec<Node>,
-    pub shapes: Vec<Shapes>,
 }
 
 pub enum Value {
@@ -31,7 +30,6 @@ impl Interpreter {
             ast: ast,
             symbol_table: HashMap::new(),
             call_stack: Vec::new(),
-            shapes: Vec::new(),
         }
     }
 
@@ -40,90 +38,39 @@ impl Interpreter {
         let statements = self.ast.statements.clone();
 
         for statement in statements {
-            self.eval(statement);
+            match statement {
+                Node::Shape(shape) => {
+                    // add shape to symbol table
+                    self.symbol_table.insert(shape.name.clone(), Value::Statements(shape.statements));
+                },
+                Node::Statement(statement) => {
+                    match statement.kind {
+                        StatementKind::DrawShape(name, properties) => {
+                            // add shape to symbol table
+                        },
+                        StatementKind::Expression(expression) => {
+                            // evaluate expression
+                            
+                        },
+                        StatementKind::Return(expression) => {
+                            // evaluate expression
+                        },
+                        StatementKind::Shift(expression, amount) => {
+                            // evaluate expression
+                            
+                        },
+                        StatementKind::Stretch(expression, amount) => {
+                            // evaluate expression
+                        },
+                        StatementKind::Rotate(expression) => {
+                            // evaluate expression
+                        },
+                    }
+                },
+                _ => {}
+            }
         }
-    }
 
-    fn eval(&mut self, statement: Node) {
-        match statement {
-            Node::Shape(shape) => {
-                // add shape to symbol table
-                self.symbol_table.insert(shape.name.clone(), Value::Statements(shape.statements));
-            },
-            Node::Statement(statement) => {
-                match statement.kind {
-                    StatementKind::DrawShape(name, properties) => {
-                        // check type of shape
-                        let shape = self.symbol_table.get(&name).unwrap();
-
-                        // check if shape is circle, rectangle, polygon, or svg
-                        match shape {
-                            Value::Shape(Shapes::Circle(circle)) => {
-                                let mut generations = 1;
-                                let mut radius = 1.0;
-                                let mut fill = (0, 0, 0);
-                                let mut outline_color = (0, 0, 0);
-                                let mut outline_width = 1.0;
-                                
-                                // evaluate properties
-                                for property in properties {
-                                    if property.name == "generations" {
-                                        generations = match *property.value {
-                                            Node::NumberLiteral(NumberLiteralNode { value }) => value as i32,
-                                            _ => 1,
-                                        }
-                                    } else if property.name == "radius" {
-                                        radius = match *property.value {
-                                            Node::NumberLiteral(NumberLiteralNode { value }) => value,
-                                            _ => 1.0,
-                                        }
-                                    }
-                                    // other properties
-                                }
-
-                                let circle = Circle::new();
-
-                                self.shapes.push(Shapes::Circle(circle));
-
-                                for _ in 0..generations {
-                                    // clone circle
-                                    // circle = circle.clone();
-
-                                    
-
-                                    // push to shapes
-
-                                }
-                            },
-                            Value::Shape(Shapes::Rectangle(rectangle)) => {
-                                unimplemented!();
-                            },
-                            Value::Shape(Shapes::Polygon(polygon)) => {
-                                unimplemented!();
-                            },
-                            Value::Shape(Shapes::SVG(svg)) => {
-                                unimplemented!();
-                            },
-                            _ => {}
-                        }
-                    },
-                    StatementKind::Expression(expression) => {
-                        // evaluate expression
-                        
-                    },
-                    StatementKind::Shift(x, y) => {
-                        // evaluate expressions that make up x and y
-                    },
-                    StatementKind::Stretch(x, y) => {
-                        // evaluate expression
-                    },
-                    StatementKind::Rotate(angle) => {
-                        // evaluate expression
-                    },
-                    _ => {}
-                }
-            },
-            _ => {}
-        }
+        
     }
 }
