af9d82c3e6103b801af3f9988a9af96ce4487655
s-shpandey <s-shpandey@lwsd.org>
2023-04-07 12:48:06 -0700

    Web assembly setup
    web assembly setup 75% done

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+mod art;
+mod ast;
+mod error;
+mod eval;
+mod lexer;
+mod parser;
+mod tokens;
+
+use crate::eval::Interpreter;
+use crate::lexer::Lexer;
+use crate::parser::Parser;
+
+use crate::art::draw;
+use crate::art::Circle;
+use crate::art::Drawable;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+use svg::node::element::path::Data;
+use svg::node::element::Path;
+use svg::Document;
+
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+
+// fn open_file(path: &str) -> Result<String, std::io::Error> {
+//     let mut file = File::open(path)?;
+//     let mut rdin = String::new();
+//     file.read_to_string(&mut rdin)?;
+//     Ok(rdin)
+// }
+
+// fn main() {
+//     let mut args = env::args();
+
+//     if args.len() > 1 {
+//         let filename: String = args.nth(1).unwrap();
+
+//         match open_file(filename.as_str()) {
+//             Ok(raw) => {
+//                 let mut lex = Lexer::new(raw);
+//                 let tokens = lex.lex();
+
+//                 // uncomment to see tokens
+//                 // for t in &tokens {
+//                 //     println!("{:#?}", t);
+//                 // }
+
+//                 let mut parser = Parser::new(tokens);
+
+//                 let ast = parser.parse_program();
+
+//                 // println!("{:#?}", ast);
+
+//                 let mut interpreter = Interpreter::new(ast);
+
+//                 interpreter.run();
+
+//                 // transform_test();
+//             }
+
+//             Err(e) => {
+//                 panic!("failed to run {}: {}", filename, e);
+//             }
+//         }
+//     }
+// }
